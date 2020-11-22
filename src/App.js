f3dcda4e318562efee15456ef974d084e8a78a37
waleedd32 <valid0@hotmail.com>
2020-11-22 23:12:41 +0200

    created Row component and printed movie posters :)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import './App.css';
 import requests from './requests'
+import Row from './Row';
 
 function App() {
   return (
-    <div className="app">
+    <div className="App">
       <h1>hello</h1>
+      <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals} />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+
     </div>
   );
 }
