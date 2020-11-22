a665ddee1f1dd9911801720b33ee769be44bc904
waleedd32 <valid0@hotmail.com>
2020-11-22 23:30:13 +0200

    made some style to rows, to look better

--- src/App.js
+++ src/App.js
@@ -5,12 +5,22 @@ import Row from './Row';
 
 function App() {
   return (
-    <div className="App">
-      <h1>hello</h1>
-      <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals} />
+    <div className="app">
+
+      <Row title="NETFLIX ORIGINALS"
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow
+      />
       <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="Action movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
 
     </div>
+
   );
 }
 
--- src/Row.js
+++ src/Row.js
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import axios from "./axios";
+import "./Row.css";
 
 const base_url = "https://image.tmdb.org/t/p/original/"
 
-function Row({ title, fetchUrl }) {
+function Row({ title, fetchUrl, isLargeRow }) {
     const [movies, setMovies] = useState([]);
 
     useEffect(() => {
@@ -25,6 +26,7 @@ function Row({ title, fetchUrl }) {
             <div className="row__posters">
                 {movies.map(movie => (
                     <img
+                        key={movie.id}
                         className="row__poster"
                         src={`${base_url}${movie.poster_path}`}
                         alt={movie.name} />
