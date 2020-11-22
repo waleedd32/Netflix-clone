011c741003c47536b9af71ed2778725bd717674e
waleedd32 <valid0@hotmail.com>
2020-11-22 22:24:40 +0200

    adding requests/endpoints

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import './App.css';
-
+import requests from './requests'
 
 function App() {
   return (
     <div className="app">
-
+      <h1>hello</h1>
     </div>
   );
 }
