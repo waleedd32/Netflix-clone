4a3a2a7e4141cf990f74bb699edd02154410ddd9
waleedd32 <valid0@hotmail.com>
2020-11-23 13:15:01 +0200

    installed react-youtube & movie-trailer modules

--- src/requests.js
+++ src/requests.js
@@ -1,5 +1,4 @@
-const API_KEY = 'Your TMDB Api_Key';
-
+const API_KEY = "Your TMDB Api_Key";
 const requests = {
     fetchTrending: `trending/all/week?api_key=${API_KEY}&language=en-US`,
     fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
