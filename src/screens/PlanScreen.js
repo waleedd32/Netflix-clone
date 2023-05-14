f9ca3cd667831502aafe5cb7759db5f859487bc4
waleedd32 <valid0@hotmail.com>
2023-05-14 18:59:26 +0300

    created PlanScreen component (here is going to be plans like 4k quality plan)

---
+++ src/screens/PlanScreen.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./PlanScreen.css";
+
+function PlanScreen() {
+  return (
+    <div className="planScreen">
+      <div className="planScreen__plan">
+        <div className="planScreen__info">
+          <h5>Premium</h5>
+          <h6>4k+HDR</h6>
+        </div>
+        <button>Subscribe</button>
+      </div>
+    </div>
+  );
+}
+
+export default PlanScreen;
