8a6d2a2a7a535f31a97b6e736aab7fdcdfb66623
waleedd32 <valid0@hotmail.com>
2023-05-18 02:26:11 +0300

    Added email and password fields to SignInScreen component

--- src/screens/SignInScreen.js
+++ src/screens/SignInScreen.js
@@ -1,7 +1,19 @@
 import React, { useRef } from "react";
 
 function SignInScreen() {
-  return <form></form>;
+  const emailRef = useRef(null);
+  const passwordRef = useRef(null);
+
+  return (
+    <form>
+      {" "}
+      <input ref={emailRef} placeholder="Email" type="email" />
+      <input ref={passwordRef} placeholder="Password" type="password" />
+      <button type="submit" onClick={signIn}>
+        Sign In
+      </button>
+    </form>
+  );
 }
 
 export default SignInScreen;
