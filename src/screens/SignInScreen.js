ca8a0654563a17103850d999861dd070a818fc98
waleedd32 <valid0@hotmail.com>
2023-05-20 01:00:13 +0300

    Added Redux dispatch in SignInScreen and implemented authentication with Firebase. Refactored form submission with signIn function. Added error handling for failed login attempts.

--- src/screens/SignInScreen.js
+++ src/screens/SignInScreen.js
@@ -1,15 +1,37 @@
 import React, { useRef } from "react";
+import { useDispatch } from "react-redux";
+import { setEmail, signInOrRegister } from "../features/userSlice";
+import { auth } from "../firebase";
 
 function SignInScreen() {
+  const dispatch = useDispatch();
+
   const emailRef = useRef(null);
   const passwordRef = useRef(null);
+
+  const signIn = (e) => {
+    e.preventDefault();
+    dispatch(setEmail(""));
+
+    auth
+      .signInWithEmailAndPassword(
+        emailRef.current.value,
+        passwordRef.current.value
+      )
+      .then((authUser) => {
+        console.log(authUser);
+      })
+      .catch((error) => alert(error.message));
+  };
 
   return (
     <form>
       <h1>Sign In</h1>
       <input ref={emailRef} placeholder="Email" type="email" />
       <input ref={passwordRef} placeholder="Password" type="password" />
-      <button type="submit">Sign In</button>
+      <button type="submit" onClick={signIn}>
+        Sign In
+      </button>
       <h4>
         <span className="signupScreen__gray">New to Jebordoq? </span>
         <span
