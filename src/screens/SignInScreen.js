aec1e91796e94d4b7c03d1a20c2d1b0579e50fb8
waleedd32 <valid0@hotmail.com>
2023-05-20 00:56:12 +0300

    Added heading and Sign Up link to SignInScreen component

--- src/screens/SignInScreen.js
+++ src/screens/SignInScreen.js
@@ -6,12 +6,20 @@ function SignInScreen() {
 
   return (
     <form>
-      {" "}
+      <h1>Sign In</h1>
       <input ref={emailRef} placeholder="Email" type="email" />
       <input ref={passwordRef} placeholder="Password" type="password" />
-      <button type="submit" onClick={signIn}>
-        Sign In
-      </button>
+      <button type="submit">Sign In</button>
+      <h4>
+        <span className="signupScreen__gray">New to Jebordoq? </span>
+        <span
+          className="signupScreen__link"
+          onClick={() => dispatch(signInOrRegister("register"))}
+        >
+          {" "}
+          Sign Up now.{" "}
+        </span>
+      </h4>
     </form>
   );
 }
