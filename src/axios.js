e071de9b442e24c3d7fcc505901023601c397bee
waleedd32 <valid0@hotmail.com>
2023-05-23 01:55:46 +0300

    removed some white spaces from css files (App.css, Banner.css)

--- src/axios.js
+++ src/axios.js
@@ -1,9 +1,8 @@
 import axios from "axios";
 
-// base url to make requests to the movie database 
+// base url to make requests to the movie database
 
 const instance = axios.create({
-    baseURL: "https://api.themoviedb.org/3"
-
-})
+  baseURL: "https://api.themoviedb.org/3",
+});
 export default instance;
