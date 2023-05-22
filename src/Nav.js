9ca1242969433f2ea39558be7d3b0ba8ac6e630a
waleedd32 <valid0@hotmail.com>
2023-05-23 02:02:47 +0300

    Update Nav component with local logo and routing

--- src/Nav.js
+++ src/Nav.js
@@ -1,35 +1,41 @@
-import React, { useState, useEffect } from 'react';
-import './Nav.css'
+import React, { useState, useEffect } from "react";
+import "./Nav.css";
+import jebordoq from "./logo/jebordoq.png";
+import { useHistory } from "react-router-dom";
 
 function Nav() {
-    const [show, handleShow] = useState(false)
+  const [show, handleShow] = useState(false);
+  const history = useHistory();
 
-    useEffect(() => {
-        window.addEventListener("scroll", () => {
-            if (window.scrollY > 100) {
-                handleShow(true);
-            } else handleShow(false);
-        })
-        return () => {
-            window.removeEventListener("scroll");
-        };
-    }, []);
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 100) {
+        handleShow(true);
+      } else handleShow(false);
+    });
+    return () => {
+      window.removeEventListener("scroll");
+    };
+  }, []);
 
-    return (
-        <div className={`nav ${show && "nav__black"}`}>
-            <img className="nav__logo"
-                src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/250px-Netflix_2015_logo.svg.png"
-                alt="Netflix Logo"
-            />
-
-        <a href="https://github.com/waleedd32/Netflix-clone"> <img
-                className="nav__avatar"
-                src="https://mir-s3-cdn-cf.behance.net/project_modules/disp/366be133850498.56ba69ac36858.png"
-                alt="Netflix Logo"
-            /></a>
- 
-        </div>
-    )
+  return (
+    <div className={`nav ${show && "nav__black"}`}>
+      <div className="nav__contents">
+        <img
+          onClick={() => history.push("/")}
+          className="nav__logo"
+          src={jebordoq}
+          alt="Jebordoq Logo"
+        />
+        <img
+          onClick={() => history.push("/profile")}
+          className="nav__avatar"
+          src="https://mir-s3-cdn-cf.behance.net/project_modules/disp/366be133850498.56ba69ac36858.png"
+          alt="Jebordoq Logo"
+        />
+      </div>
+    </div>
+  );
 }
 
-export default Nav
+export default Nav;
