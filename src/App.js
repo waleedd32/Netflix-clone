ec319e31e84cf39a51607f5a49f66bca2019ef78
waleedd32 <valid0@hotmail.com>
2023-05-23 02:08:59 +0300

    Integrated authentication and routing with Redux and React Router

--- src/App.js
+++ src/App.js
@@ -1,27 +1,56 @@
-import React from 'react'
-import './App.css';
-import requests from './requests'
-import Row from './Row';
-import Banner from './Banner'
-import Nav from './Nav';
+import React, { useEffect } from "react";
+import "./App.css";
+import HomeScreen from "./screens/HomeScreen";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import LoginScreen from "./screens/LoginScreen";
+import { auth } from "./firebase";
+import { useDispatch, useSelector } from "react-redux";
+import { login, logout, selectUser } from "./features/userSlice";
+import ProfileScreen from "./screens/ProfileScreen";
 
 function App() {
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    //this listens to any authenticated state changes
+    // firebase store it to local memory (it knows that you logged in before)
+    const unsubscribe = auth.onAuthStateChanged((userAuth) => {
+      if (userAuth) {
+        // logged in
+        console.log(userAuth);
+        dispatch(
+          login({
+            uid: userAuth.uid,
+            email: userAuth.email,
+          })
+        );
+      } else {
+        // logged out
+        dispatch(logout());
+      }
+    });
+    return unsubscribe;
+  }, [dispatch]);
+
   return (
     <div className="app">
-      <Nav />
-      <Banner />
-      <Row title="NETFLIX ORIGINALS"
-        fetchUrl={requests.fetchNetflixOriginals}
-        isLargeRow
-      />
-      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
-      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
-      <Row title="Action movies" fetchUrl={requests.fetchActionMovies} />
-      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
-      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
-      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
-      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
+      <Router>
+        {!user ? (
+          <LoginScreen />
+        ) : (
+          <Switch>
+            <Route exact path="/profile">
+              <ProfileScreen />
+            </Route>
+            <Route exact path="/">
+              <HomeScreen />
+            </Route>
+          </Switch>
+        )}
+      </Router>
     </div>
   );
 }
+
 export default App;
--- src/Row.js
+++ src/Row.js
@@ -1,63 +1,66 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import axios from "./axios";
 import "./Row.css";
 import Youtube from "react-youtube";
-import movieTrailer from 'movie-trailer'
+import movieTrailer from "movie-trailer";
 
-const base_url = "https://image.tmdb.org/t/p/original/"
+const base_url = "https://image.tmdb.org/t/p/original/";
 
 function Row({ title, fetchUrl, isLargeRow }) {
-    const [movies, setMovies] = useState([]);
-    const [trailerUrl, setTrailerUrl] = useState("")
+  const [movies, setMovies] = useState([]);
+  const [trailerUrl, setTrailerUrl] = useState("");
 
-    useEffect(() => {
-
-        async function fetchData() {
-            const request = await axios.get(fetchUrl);
-            // full url
-            setMovies(request.data.results);
-            return request;
-        }
-        fetchData();
-    }, [fetchUrl]);
-
-    const opts = {
-        height: "390",
-        width: "100%",
-        playerVars: {
-            autoplay: 1,
-        },
-    };
-    const handleClick = (movie) => {
-        console.log(movie)
-
-        if (trailerUrl) {
-            setTrailerUrl('');
-        } else {
-            movieTrailer(movie?.name || "")
-                .then((url) => {
-                    // full url like: https://www.youtube.com/watch?v=_x6lux6f_6g&t=1s
-                    const urlParams = new URLSearchParams(new URL(url).search);
-                    setTrailerUrl(urlParams.get('v'));
-                }).catch(error => console.log(error))
-        }
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(fetchUrl);
+      // full url
+      setMovies(request.data.results);
+      return request;
     }
-    return (
-        <div className="row">
-            <h2>{title}</h2>
-            <div className="row__posters">
-                {movies.map(movie => (
-                    <img
-                        key={movie.id}
-                        onClick={() => handleClick(movie)}
-                        className={`row__poster ${isLargeRow && "row__posterLarge"}`}
-                        src={`${base_url}${isLargeRow ? movie.poster_path : movie.backdrop_path}`}
-                        alt={movie.name} />
-                ))}
-            </div>
-            {trailerUrl && <Youtube videoId={trailerUrl} opts={opts} />}
-        </div>
-    )
+    fetchData();
+  }, [fetchUrl]);
+
+  const opts = {
+    height: "390",
+    width: "100%",
+    playerVars: {
+      autoplay: 1,
+    },
+  };
+  const handleClick = (movie) => {
+    console.log(movie);
+
+    if (trailerUrl) {
+      setTrailerUrl("");
+    } else {
+      movieTrailer(movie?.name || "")
+        .then((url) => {
+          // full url like: https://www.youtube.com/watch?v=_x6lux6f_6g&t=1s
+          const urlParams = new URLSearchParams(new URL(url).search);
+          setTrailerUrl(urlParams.get("v"));
+        })
+        .catch((error) => console.log(error));
+    }
+  };
+  return (
+    <div className="row">
+      <h2>{title}</h2>
+      <div className="row__posters">
+        {movies.map((movie) => (
+          <img
+            key={movie.id}
+            onClick={() => handleClick(movie)}
+            className={`row__poster ${isLargeRow && "row__posterLarge"}`}
+            src={`${base_url}${
+              isLargeRow ? movie.poster_path : movie.backdrop_path
+            }`}
+            alt={movie.name}
+          />
+        ))}
+      </div>
+      {trailerUrl && <Youtube videoId={trailerUrl} opts={opts} />}
+    </div>
+  );
 }
 
 export default Row;
